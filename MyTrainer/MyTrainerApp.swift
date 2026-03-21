5dca7c152984e5a99c2640733e2f19fa5241b8c1
Nando <fernando.deleon@gmail.com>
2026-03-21 20:49:52 +1100

    Initial Commit

---
+++ MyTrainer/MyTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTrainerApp.swift
+//  MyTrainer
+//
+//  Created by Fernando De Leon on 21/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
